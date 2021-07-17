0fe85a7330404ca89feb9912c3631d4e015267ec
Singularity0909 <904854724@qq.com>
2021-07-18 04:18:53 +0800

    refactor: conf/mysql.go

---
+++ conf/mysql.go
@@ -0,0 +1,34 @@
+package conf
+
+import (
+	"io/ioutil"
+
+	"github.com/bytedance/gopkg/util/logger"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	_DB_CONF_PATH = "conf/mysql.yml"
+)
+
+type DBConf struct {
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Hostname string `yaml:"hostname"`
+	Port     int    `yaml:"port"`
+	Name     string `yaml:"name"`
+}
+
+func GetDBConf() (*DBConf, error) {
+	conf := new(DBConf)
+	file, err := ioutil.ReadFile(_DB_CONF_PATH)
+	if err != nil {
+		logger.Errorf("[GetDBConf] open file failed. err:%v", err)
+		return nil, err
+	}
+	if err = yaml.Unmarshal(file, conf); err != nil {
+		logger.Errorf("[GetDBConf] unmarshal file failed. err:%v", err)
+		return nil, err
+	}
+	return conf, nil
+}
