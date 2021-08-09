df36d089d519f74efde785626010c4e68c48afd9
Singularity0909 <904854724@qq.com>
2021-08-10 00:19:40 +0800

    feat: log middleware

--- client/consul.go
+++ client/consul.go
@@ -3,11 +3,11 @@ package client
 import (
 	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
 	"sync/atomic"
 
 	"github.com/ameidance/paster_core/constant"
+	"github.com/ameidance/paster_core/frame"
 	"github.com/ameidance/paster_core/util"
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/registry"
@@ -16,9 +16,9 @@ import (
 )
 
 var (
-	ConsulCheckCounter int64
-	ConsulConf         *_ConsulConf
-	ConsulClient       *api.Client
+	consulCheckCounter int64
+	consulConf         *_ConsulConf
+	consulClient       *api.Client
 )
 
 type _ConsulConf struct {
@@ -29,42 +29,37 @@ type _ConsulConf struct {
 
 func InitConsul() {
 	var err error
-	ConsulConf, err = getConsulConfig()
-	if ConsulConf == nil || err != nil {
+	consulConf, err = getConsulConfig()
+	if consulConf == nil || err != nil {
 		panic(err)
 	}
 	config := api.DefaultConfig()
-	config.Address = fmt.Sprintf("%v:%v", ConsulConf.Hostname, ConsulConf.Port)
-	ConsulClient, err = api.NewClient(config)
-	if ConsulClient == nil || err != nil {
+	config.Address = fmt.Sprintf("%v:%v", consulConf.Hostname, consulConf.Port)
+	consulClient, err = api.NewClient(config)
+	if consulClient == nil || err != nil {
 		panic(err)
 	}
-	go healthCheckHandler(ConsulConf.CheckPort)
+	go healthCheckHandler(consulConf.CheckPort)
 }
 
 type ConsulRegistry struct {
-	ServiceName string
-	InstanceId  string
 }
 
 func NewConsulRegistry() *ConsulRegistry {
-	if ConsulConf == nil {
+	if consulConf == nil {
 		return nil
 	}
-	return &ConsulRegistry{
-		ServiceName: constant.SERVICE_NAME,
-		InstanceId:  fmt.Sprintf("%s-%d", constant.SERVICE_NAME, rand.Int()),
-	}
+	return &ConsulRegistry{}
 }
 
 func (m *ConsulRegistry) Register(info *registry.Info) (err error) {
-	if ConsulClient == nil {
+	if consulClient == nil {
 		return nil
 	}
 
 	registration := new(api.AgentServiceRegistration)
-	registration.ID = m.InstanceId
-	registration.Name = m.ServiceName
+	registration.ID = frame.GetInstanceId()
+	registration.Name = frame.GetServiceName()
 	registration.Address = util.GetInternalIP()
 	_, err = fmt.Sscanf(info.Addr.String(), ":%v", &registration.Port)
 	if err != nil {
@@ -73,22 +68,22 @@ func (m *ConsulRegistry) Register(info *registry.Info) (err error) {
 	}
 
 	registration.Check = new(api.AgentServiceCheck)
-	registration.Check.HTTP = fmt.Sprintf("http://%s:%d/health", registration.Address, ConsulConf.CheckPort)
+	registration.Check.HTTP = fmt.Sprintf("http://%s:%d/health", registration.Address, consulConf.CheckPort)
 	registration.Check.Timeout = "5s"
 	registration.Check.Interval = "5s"
 	registration.Check.DeregisterCriticalServiceAfter = "10s"
 
-	klog.Infof("[ConsulRegistry -> Register] registering... instance id:%v", m.InstanceId)
-	return ConsulClient.Agent().ServiceRegister(registration)
+	klog.Infof("[ConsulRegistry -> Register] registering... instance id:%v", frame.GetInstanceId())
+	return consulClient.Agent().ServiceRegister(registration)
 }
 
 func (m *ConsulRegistry) Deregister(info *registry.Info) error {
-	if ConsulClient == nil {
+	if consulClient == nil {
 		return nil
 	}
 
-	klog.Infof("[ConsulRegistry -> Register] deregistering... instance id:%v", m.InstanceId)
-	return ConsulClient.Agent().ServiceDeregister(m.InstanceId)
+	klog.Infof("[ConsulRegistry -> Register] deregistering... instance id:%v", frame.GetInstanceId())
+	return consulClient.Agent().ServiceDeregister(frame.GetInstanceId())
 }
 
 func getConsulConfig() (*_ConsulConf, error) {
@@ -107,8 +102,8 @@ func getConsulConfig() (*_ConsulConf, error) {
 
 func healthCheckHandler(port int) {
 	http.HandleFunc("/health", func(writer http.ResponseWriter, request *http.Request) {
-		atomic.AddInt64(&ConsulCheckCounter, 1)
-		klog.Infof("[healthCheckHandler] counter:%v", atomic.LoadInt64(&ConsulCheckCounter))
+		atomic.AddInt64(&consulCheckCounter, 1)
+		//klog.Debugf("[healthCheckHandler] counter:%v", atomic.LoadInt64(&consulCheckCounter))
 	})
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		klog.Errorf("[healthCheckHandler] serve failed. err:%v", err)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/ameidance/paster_core/client"
+	"github.com/ameidance/paster_core/frame"
 	"github.com/ameidance/paster_core/model/dto/kitex_gen/ameidance/paster/core/pastercoreservice"
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/server"
@@ -11,7 +12,8 @@ func main() {
 	client.InitDB()
 	client.InitConsul()
 
-	srv := pastercoreservice.NewServer(new(PasterCoreServiceImpl), server.WithRegistry(client.NewConsulRegistry()))
+	srv := pastercoreservice.NewServer(new(PasterCoreServiceImpl), server.WithServerBasicInfo(frame.EBI),
+		server.WithRegistry(client.NewConsulRegistry()), server.WithMiddleware(frame.LogMiddleware))
 	if err := srv.Run(); err != nil {
 		klog.Errorf("[main] server stopped with error. err:%v", err)
 		panic(err)
