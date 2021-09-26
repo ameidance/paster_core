4bd544561f06259fb706df69d3bf6859d83f6bbc
Singularity0909 <904854724@qq.com>
2021-09-26 17:39:10 +0800

    chore: update kitex

--- client/consul.go
+++ client/consul.go
@@ -56,7 +56,7 @@ func (m *ConsulRegistry) Register(info *registry.Info) (err error) {
 	registration.ID = frame.GetInstanceId()
 	registration.Name = frame.GetServiceName()
 	registration.Address = util.GetInternalIP()
-	_, err = fmt.Sscanf(info.Addr.String(), ":%v", &registration.Port)
+	_, err = fmt.Sscanf(info.Addr.String(), "[::]:%v", &registration.Port)
 	if err != nil {
 		klog.Errorf("[ConsulRegistry -> Register] get registry info addr port failed. err:%v", err)
 		return
